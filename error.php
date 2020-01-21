017feea63acec93b65b958155388eae57762bf3e
ThibaultLIMMOIS <thibault.limmois@gmail.com>
2020-01-21 12:50:00 +0100

    Update 404 page

--- error.php
+++ error.php
@@ -3,6 +3,6 @@ switch($_GET['code'])
 {
 case '404':header('Location: https://qwanturank.life/404.php');
 break;
-default:header('Location: https://qwanturank.life/');
+default:header('Location: https://qwanturank.life/404.php');
 }
 ?>
