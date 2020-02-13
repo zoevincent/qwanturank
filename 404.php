527fb920d53874571f675fb8a9b0680588aa1904
ThibaultLIMMOIS <thibault.limmois@gmail.com>
2020-02-13 19:11:41 +0100

    Add 404 page CSS

--- 404.php
+++ 404.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <title>Page d'erreur 404</title>
+        <link rel="stylesheet" href="styles/404.css">
     </head>
     <body>
         <h1>Oups! Page introuvable</h1>
         <p>Vous avez tenté d’accéder à une page qui ne se trouve plus sur ce site. Veuillez revenir sur la page d'accueil afin de continuer votre navigation sur notre site.</p>
-        <p><a href="https://www.qwanturank.life">Revenir à l'accueil</a></p>
+        <a href="https://www.qwanturank.life">Revenir à l'accueil</a>
     </body>
 </html>
